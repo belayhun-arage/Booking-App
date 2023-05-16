0a8cb4b51ce52b49ba1fed277fe640f080976e9d
belayhun-arage <belayhun24arage@gmail.com>
2023-05-16 21:41:44 +0300

    working on token verification

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,7 +1,12 @@
 import Express from "express";
 import {login } from "../controllers/auth.js";
+import { verifyToken } from "../utils/verifyToken.js";
 const router = Express.Router()
 
+
+router.get('/auth',verifyToken,(req,res,next)=>{
+    res.send(200).json({message:"Token Verified"})
+})
 router.post('/authUser',login)   
 
 export default router;
---
+++ api/utils/verifyToken.js
@@ -0,0 +1,14 @@
+import Jwt from "jsonwebtoken";
+import createError from "../utils/error";
+
+export const verifyToken=(req,res,next)=>{
+    const token=req.cookies.access_token
+    if(!token) {
+        return next(createError(401,"User Not Authenticated"))
+    }
+    Jwt.verify(token,process.env.JWT_SECRETE_KEY,(err,user)=>{
+        if(err) return next(createError(403,"Not Valid Token"))
+        req.user=user
+        next()
+    })
+}
